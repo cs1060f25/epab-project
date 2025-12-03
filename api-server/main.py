6f341ed51033fa67b21cdd90c166b52697c4bf36
Eli Pinkus <epinkus@mba2026.hbs.edu>
2025-12-03 14:06:52 -0500

    Add tests to reproduce CORS preflight bug (CS12-48)

--- api-server/main.py
+++ api-server/main.py
@@ -43,7 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
